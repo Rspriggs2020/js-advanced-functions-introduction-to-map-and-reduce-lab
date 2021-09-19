270cdd047dec39bee28845448b645d7d8559fa90
ryliespriggs <rspriggs812@gmail.com>
2021-09-19 10:52:11 -0700

    commit tree f055683d3c5b007f482876253b6596e98f2cfefa

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+function mapToNegativize(number) {
+    let array = []
+    for (let i = 0; i < number.length; i++) {
+        array.push(-1 * number[i])
+    };
+    return array
+}
+
+function mapToNoChange(number) {
+    let array = []
+    for (let i = 0; i < number.length; i++) {
+        array.push(number[i])
+    };
+    return array
+}
+
+function mapToDouble(number) {
+    let array = []
+    for (let i = 0; i < number.length; i++) {
+    array.push(2 * number[i]) 
+    };
+    return array
+}
+
+function mapToSquare(number) {
+    let array = []
+    for (let i = 0; i < number.length; i++) {
+    array.push(number[i] * number[i]) 
+    };
+    return array
+}
+
+//reduce
+
+function reduceToTotal(number, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < number.length; i++) {
+        total = total + number[i]
+    };
+    return total
+}
+
+function reduceToAllTrue(number) {
+    for (let i = 0; i < number.length; i++) {
+        if (!number[i]) return false
+    };
+    return true
+}
+
+function reduceToAnyTrue(number) {
+    for (let i = 0; i < number.length; i++) {
+        if (number[i]) return true
+    };
+    return false
+}
